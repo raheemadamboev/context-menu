59cb663bd92184363302705dd30eee24e0531b0a
raheemadamboev <vividricemaryandmax@gmail.com>
2023-04-27 20:00:58 +0500

    Added People;

---
+++ app/src/main/java/xyz/teamgravity/contextmenu/People.kt
@@ -0,0 +1,160 @@
+package xyz.teamgravity.contextmenu
+
+object People {
+    val VALUE = listOf(
+        PersonModel(
+            name = "Raheem",
+            menus = listOf(
+                PersonMenuModel(
+                    title = "Menu 1"
+                ),
+                PersonMenuModel(
+                    title = "Menu 2"
+                ),
+                PersonMenuModel(
+                    title = "Menu 3"
+                ),
+            )
+        ),
+        PersonModel(
+            name = "Tupac",
+            menus = listOf(
+                PersonMenuModel(
+                    title = "Menu 1"
+                ),
+                PersonMenuModel(
+                    title = "Menu 2"
+                ),
+                PersonMenuModel(
+                    title = "Menu 3"
+                ),
+            )
+        ),
+        PersonModel(
+            name = "Eminem",
+            menus = listOf(
+                PersonMenuModel(
+                    title = "Menu 1"
+                ),
+                PersonMenuModel(
+                    title = "Menu 2"
+                ),
+                PersonMenuModel(
+                    title = "Menu 3"
+                ),
+            )
+        ),
+        PersonModel(
+            name = "Ronaldo",
+            menus = listOf(
+                PersonMenuModel(
+                    title = "Menu 1"
+                ),
+                PersonMenuModel(
+                    title = "Menu 2"
+                ),
+                PersonMenuModel(
+                    title = "Menu 3"
+                ),
+            )
+        ),
+        PersonModel(
+            name = "Elon",
+            menus = listOf(
+                PersonMenuModel(
+                    title = "Menu 1"
+                ),
+                PersonMenuModel(
+                    title = "Menu 2"
+                ),
+                PersonMenuModel(
+                    title = "Menu 3"
+                ),
+            )
+        ),
+        PersonModel(
+            name = "Andrew",
+            menus = listOf(
+                PersonMenuModel(
+                    title = "Menu 1"
+                ),
+                PersonMenuModel(
+                    title = "Menu 2"
+                ),
+                PersonMenuModel(
+                    title = "Menu 3"
+                ),
+            )
+        ),
+        PersonModel(
+            name = "Tristian",
+            menus = listOf(
+                PersonMenuModel(
+                    title = "Menu 1"
+                ),
+                PersonMenuModel(
+                    title = "Menu 2"
+                ),
+                PersonMenuModel(
+                    title = "Menu 3"
+                ),
+            )
+        ),
+        PersonModel(
+            name = "Messi",
+            menus = listOf(
+                PersonMenuModel(
+                    title = "Menu 1"
+                ),
+                PersonMenuModel(
+                    title = "Menu 2"
+                ),
+                PersonMenuModel(
+                    title = "Menu 3"
+                ),
+            )
+        ),
+        PersonModel(
+            name = "Robben",
+            menus = listOf(
+                PersonMenuModel(
+                    title = "Menu 1"
+                ),
+                PersonMenuModel(
+                    title = "Menu 2"
+                ),
+                PersonMenuModel(
+                    title = "Menu 3"
+                ),
+            )
+        ),
+        PersonModel(
+            name = "Skies",
+            menus = listOf(
+                PersonMenuModel(
+                    title = "Menu 1"
+                ),
+                PersonMenuModel(
+                    title = "Menu 2"
+                ),
+                PersonMenuModel(
+                    title = "Menu 3"
+                ),
+            )
+        ),
+        PersonModel(
+            name = "Joe",
+            menus = listOf(
+                PersonMenuModel(
+                    title = "Menu 1"
+                ),
+                PersonMenuModel(
+                    title = "Menu 2"
+                ),
+                PersonMenuModel(
+                    title = "Menu 3"
+                ),
+            )
+        ),
+    )
+}
